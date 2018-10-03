590add2c3298ec4135348d16c59e7fd16f05c0e1
William B McAllister <blakemcallister77@gmail.com>
2018-10-02 21:55:38 -0400

    pg 111

---
+++ threeMethodExercise/threeMethodExercise/MathClass.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace threeMethodExercise
+{
+	public class MathClass
+	{
+		public static int AddUserNumber(int i)
+		{
+			return i + 9;
+		}
+
+		public static int SubUserNumber(int k)
+		{
+			return k - 5;
+		}
+
+		public static int TimesUserNumber(int j)
+		{
+			return j * 3;
+		}
+	}
+}
---
+++ threeMethodExercise/threeMethodExercise/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace threeMethodExercise
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Please enter a number between 1-50.");
+			int userNumber = Convert.ToInt32(Console.ReadLine());
+			int addedUserNumber = MathClass.AddUserNumber(userNumber);
+			int minusUserNumber = MathClass.SubUserNumber(userNumber);
+			int timesUserNumber = MathClass.TimesUserNumber(userNumber);
+			Console.WriteLine("Your number plus 9 is " + addedUserNumber);
+			Console.WriteLine("Your number minus 5 is " + minusUserNumber);
+			Console.WriteLine("Your number times 3 is " + timesUserNumber);
+			Console.WriteLine("Thanks for playing along.");
+			Console.ReadLine();
+		}
+		
+		
+
+	
+		
+		
+
+
+		
+		
+		
+	}
+}
